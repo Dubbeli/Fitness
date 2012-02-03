d07ccee91f88afb14b1a93a53a34c1049ec5ce50
Adri Wiekens <a3_wiekens@hotmail.com>
2012-02-03 19:52:39 +0100

    basic rails project without any content

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fitness::Application.config.secret_token = '9718f1244e9e6e1ef207c895f21693c7ec61ea68d3882cdd8240a41ebc045d3e6904d847da1c477d66b175da942d294c7eb69ca764cc29ef499090e657d03bb4'
